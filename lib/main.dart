4d8038ac5d0177e809e74b05b4ae288257593f6f
Grant Commodore <grantcommodore24@gmail.com>
2020-05-14 13:53:13 -0400

    Added popup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import './widgets/list_of_transactions.dart';
+import './widgets/new_transactions.dart';
+import './models/transaction.dart';
 
 void main() {
   runApp(ExpenseApp());
@@ -16,8 +18,18 @@ class ExpenseApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget { 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: "New Boots", amount: 200.00, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: "New Laptop", amount: 1633.00, date: DateTime.now())
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,26 +40,53 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.blueGrey,
         centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            color: Colors.orange,
+            onPressed: (){
+              _startNewTransaction(context); 
+            },
+          )
+        ],
       ),
       body: SingleChildScrollView(
-                  child: Column(
-            children: <Widget>[
-              Card(
-                color: Colors.blueGrey,
-                child: Container(
-                    width: double.infinity,
-                    height: 100,
-                    child: Center(
-                        child: Text(
-                      "Chart",
-                    ))),
-                elevation: 5,
-              ),
-              UserTransactions(), 
-            ],
-          ),
+        child: Column(
+          children: <Widget>[
+            Card(
+              color: Colors.blueGrey,
+              child: Container(
+                  width: double.infinity,
+                  height: 100,
+                  child: Center(
+                      child: Text(
+                    "Chart",
+                  ))),
+              elevation: 5,
+            ),
+           NewTransaction(_addNewTransaction), 
+           TransactionList(_userTransactions),
+          ],
         ),
-      
+      ),
     );
+  }
+
+  void _startNewTransaction(BuildContext ctx){
+    showModalBottomSheet(context: ctx, builder: (_){
+      return NewTransaction(_addNewTransaction); 
+    }); 
+  }
+
+  void _addNewTransaction(String NTitle, double NAmount) {
+    final newTx = Transaction(
+        title: NTitle,
+        amount: NAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
   }
 }
--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,40 +1,65 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
 
-  final inputTitle = TextEditingController(); 
-  final inputAmount = TextEditingController(); 
-  final Function addTx; 
 
-  NewTransaction(this.addTx); 
+  NewTransaction(this.addTx);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final inputTitle = TextEditingController();
+
+  final inputAmount = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Card(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: "Title"),
-                      controller: inputTitle,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: "Amount"),
-                      controller: inputAmount,
-                    ),
-                    FlatButton(
-                      child: Text(
-                        "Add Transaction",
-                        style: TextStyle(color: Colors.orange),
-                      ),
-                      onPressed: (){
-                        addTx(inputTitle.text, double.parse(inputAmount.text));  
-                      },
-                    )
-                  ],
-                ),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: "Title"),
+              controller: inputTitle,
+              onSubmitted: (_) {
+                _submitTransaction();
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Amount"),
+              controller: inputAmount,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) {
+                _submitTransaction();
+              },
+            ),
+            FlatButton(
+              child: Text(
+                "Add Transaction",
+                style: TextStyle(color: Colors.orange),
               ),
-            );
+              onPressed: () {
+                _submitTransaction;
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _submitTransaction() {
+    final enteredTitle = inputTitle.text; 
+    final enteredAmount = double.parse(inputAmount.text); 
+    if(enteredTitle.isEmpty || enteredAmount < 0){
+      return; 
+    }
+    widget.addTx(inputTitle.text, double.parse(inputAmount.text));
+
+    Navigator.of(context).pop();
   }
 }
